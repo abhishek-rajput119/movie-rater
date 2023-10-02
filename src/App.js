dca892de83c5f6108190f85b5e72533d105c32fd
abhishek.rajput <abrajput119@gmail.com>
2023-10-02 09:54:48 +0530

    adding cors and rating functionality

--- src/App.js
+++ src/App.js
@@ -1,29 +1,42 @@
-import { useState } from 'react';
-import './App.css';
-
-export const MovieList = () => {
-  const [movies, setMovies] = useState(['Movie 1', 'Movie 2'])
-  return (
-    <div className='layout'>
-      <div>
-        {movies.map(movie=>{
-          return <h2 key={movie
-          }>{movie}</h2>
-        })}
-      </div>
-      <div>Movie Details</div>
-    </div>
-  )
-}
+import { useEffect, useState } from 'react'
+import './App.css'
+import MovieList from './components/MovieList'
+import MovieDetails from './components/MovieDetails'
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/api/movies/', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Token dc00d08c75cf058d4c17c8f61a6ef1c974430807',
+      },
+    })
+      .then((resp) => resp.json())
+      .then((resp) => setMovies(resp))
+      .catch((error) => console.log(error))
+  }, [])
+
+  const selectMovie = (movie)=>{
+    setSelectedMovie(movie)
+  }
+
+const loadMovie = (movie) => {
+  setSelectedMovie(movie)
+}
+  
   return (
     <div className="App">
       <header className="App-header">
         <h1>Movie Rater</h1>
       </header>
-      <MovieList />
+      <div className="layout">
+        <MovieList movies={movies} selectMovie={selectMovie}/>
+        <MovieDetails movie={selectedMovie} updateMovie={loadMovie}/>
+      </div>
     </div>
   )
 }
 
-export default App;
+export default App
